643124daa21093f9a9cc7ba040589bb75c7450b1
Ryan Hodin <ryan951357@gmail.com>
2018-02-28 00:58:06 -0600

    Implement CheckIntersect
    Returns 1 if the given bounding boxes intersect, or 0 otherwise

--- game.asm
+++ game.asm
@@ -19,25 +19,80 @@ include game.inc
 ;; Has keycodes
 include keys.inc
 
-	
+
 .DATA
 
 ;; If you need to, you can place global variables here
 
 
 .CODE
-	
 
-;; Note: You will need to implement CheckIntersect!!!
+;; Returns the absolute value of a
+AbsoluteValue PROC a:SDWORD
+
+    mov eax, a
+    cmp eax, 0
+    jge SKIP
+    neg eax
+SKIP:
+    ret
+AbsoluteValue ENDP
+
+CheckIntersect PROC USES ebx, ecx oneX:DWORD, oneY:DWORD, oneBitmap:PTR EECS205BITMAP, twoX:DWORD, twoY:DWORD, twoBitmap:PTR EECS205BITMAP
+
+    ;; ebx contains the sum of halfwidths as a DWORD (not FXPT)
+    INVOKE ToFixedPoint, (EECS205BITMAP PTR [oneBitmap]).dwWidth
+    INVOKE FixedMultiply, eax, HALF
+    mov ebx, eax
+    INVOKE ToFixedPoint, (EECS205BITMAP PTR [twoBitmap]).dwWidth
+    INVOKE FixedMultiply, eax, HALF
+    INVOKE FixedAdd, ebx, eax
+    INVOKE FromFixedPoint, eax
+    mov ebx, eax
+
+    ;; ecx contains the sum of halfheights
+    INVOKE ToFixedPoint, (EECS205BITMAP PTR [oneBitmap]).dwHeight
+    INVOKE FixedMultiply, eax, HALF
+    mov ecx, eax
+    INVOKE ToFixedPoint, (EECS205BITMAP PTR [twoBitmap]).dwHeight
+    INVOKE FixedMultiply, eax, HALF
+    INVOKE FixedAdd, ecx, eax
+    INVOKE FromFixedPoint, eax
+    mov ecx, eax
+
+    ;; If the difference between xcenters is greater than or equal to ebx, return 0
+    mov eax, twoX
+    sub eax, oneX
+    INVOKE AbsoluteValue, eax
+    cmp eax, ebx
+    jl FALSE
+
+    ;; If the difference between ycenters is greater than or equal to ecx, return 0
+    mov eax, twoY
+    sub eax, oneY
+    INVOKE AbsoluteValue, eax
+    cmp eax, ecx
+    jl FALSE
+
+    ;; If we get here, then there exists some overlap
+    mov eax, 1
+    jmp EXIT
+
+FALSE:
+    mov eax, 0
+    ;; Fallthrough
+EXIT:
+    ret
+CheckIntersect ENDP
 
 GameInit PROC
-	
+
 	ret         ;; Do not delete this line!!!
 GameInit ENDP
 
 
 GamePlay PROC
-	
+
 	ret         ;; Do not delete this line!!!
 GamePlay ENDP
 
