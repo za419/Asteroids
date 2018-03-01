5e20acfc956fde0127eb21864abff9986342d5ea
Ryan Hodin <ryan951357@gmail.com>
2018-03-01 03:12:10 -0600

    Fix blank for left-facing bitmap

--- blit.asm
+++ blit.asm
@@ -91,6 +91,7 @@ ERROR: ;; Just return on error...
 	ret 			; Don't delete this line!!!
 DrawPixel ENDP
 
+;; Bitmap with zero rotation
 BasicBlit PROC USES eax ebx ecx edx esi edi ptrBitmap:PTR EECS205BITMAP , xcenter:DWORD, ycenter:DWORD
     LOCAL halfwidth:SDWORD, halfheight:SDWORD, dstWidth:DWORD, dstX:DWORD, dstY:DWORD
 
@@ -211,6 +212,7 @@ DONE: ;; Exiting the outer loop is just returning from the function
     ret
 BasicBlit ENDP
 
+;; Bitmap with 180 degrees of rotation
 UpsideDownBlit PROC USES eax ebx ecx edx esi edi ptrBitmap:PTR EECS205BITMAP , xcenter:DWORD, ycenter:DWORD
     LOCAL halfwidth:SDWORD, halfheight:SDWORD, dstWidth:DWORD, dstX:DWORD, dstY:DWORD
 
@@ -331,6 +333,128 @@ DONE: ;; Exiting the outer loop is just returning from the function
     ret
 UpsideDownBlit ENDP
 
+;; Bitmap with 90 degrees of rotation
+LeftBlit PROC USES eax ebx ecx edx esi edi ptrBitmap:PTR EECS205BITMAP , xcenter:DWORD, ycenter:DWORD
+    LOCAL halfwidth:SDWORD, halfheight:SDWORD, dstWidth:DWORD, dstX:DWORD, dstY:DWORD
+
+    ;; Use a register for faster lookup
+    mov esi, ptrBitmap
+
+    ;; Fill in local variables
+    ;; halfwidth
+    mov eax, (EECS205BITMAP PTR [esi]).dwWidth
+    shr eax, 1
+    mov halfwidth, eax
+
+    ;; halfheight
+    mov eax, (EECS205BITMAP PTR [esi]).dwHeight
+    shr eax, 1
+    mov halfheight, eax
+
+    ;; dstWidth
+    mov eax, (EECS205BITMAP PTR [esi]).dwWidth
+    add eax, (EECS205BITMAP PTR [esi]).dwHeight
+    mov dstWidth, eax
+
+    ;; For loops
+    ;; These are in the wrong order for proper cache coherency. But oh well
+    ;; Outer loop initializer
+    mov dstX, eax
+    neg dstX
+OUTER: ;; Outer loop condition
+    mov eax, dstX
+    cmp eax, dstWidth
+    jge DONE
+
+    ;; Inner loop initializer
+    mov eax, dstWidth
+    neg eax
+    mov dstY, eax
+INNER: ;; Inner loop condition
+    mov eax, dstY
+    cmp eax, dstWidth
+    jge OINCR ;; Jump to outer loop increment
+
+    ;; Inner loop
+    ;; Big condition chain on whether to draw
+    ;; If any of the conditions are false (since they are all AND'd together), skip to the inner increment (IINCR)
+    ;; If dstX<0, do not draw
+    mov edx, dstX
+    cmp edx, 0
+    jl IINCR
+
+    ;; if dstX>=bitmap width, do not draw
+    cmp edx, (EECS205BITMAP PTR [esi]).dwWidth
+    jge IINCR
+
+    ;; If dstY<0, do not draw
+    mov ebx, dstY
+    cmp ebx, 0
+    jl IINCR
+
+    ;; IF dstY>=bitmap height, do not draw
+    cmp ebx, (EECS205BITMAP PTR [esi]).dwHeight
+    jge IINCR
+
+    ;; If (xcenter+dstX-halfwidth)<0, do not draw
+    mov eax, xcenter
+    add eax, dstX
+    sub eax, halfwidth
+    cmp eax, 0
+    jl IINCR
+
+    ;; IF (xcenter+dstX-halfwidth)>=639, do not draw
+    cmp eax, SCREEN_WIDTH-1
+    jge IINCR
+
+    ;; If (ycenter+dstY-halfheight)<0, do not draw
+    mov eax, ycenter
+    add eax, dstY
+    sub eax, halfheight
+    cmp eax, 0
+    jl IINCR
+
+    ;; If (ycenter+dstY-halfheight)>=479, do not draw
+    cmp eax, SCREEN_HEIGHT-1
+    jge IINCR
+
+    ;; If the target pixel is not transparent
+    INVOKE Index2D, dstX, dstY, (EECS205BITMAP PTR [esi]).dwWidth
+    ;; eax holds the proper index into the array of pixels
+    mov ecx, (EECS205BITMAP PTR [esi]).lpBytes
+    mov al, BYTE PTR [ecx+eax] ;; al holds the bitmap pixel
+    cmp al, (EECS205BITMAP PTR [esi]).bTransparent
+    je IINCR
+
+    ;; If we're here, then we're allowed to draw
+    ;; We'll use ebx for x coordinate, and edx for y coordinate
+    ;; al holds our color value
+    ;; Compute x coordinate
+    mov ebx, xcenter
+    add ebx, dstY
+    sub ebx, halfheight
+
+    ;; Compute y coordinate
+    mov edx, ycenter
+    sub edx, dstX
+    add edx, halfwidth
+
+    ;; Now draw
+    INVOKE DrawPixel, ebx, edx, al
+    ;; Fallthrough to the increment
+IINCR: ;; Inner increment
+    inc dstY
+    jmp INNER
+
+OINCR: ;; Outer increment
+    inc dstX
+    jmp OUTER
+
+DONE: ;; Exiting the outer loop is just returning from the function
+    ret
+LeftBlit ENDP
+
+;; Bitmap with arbitrary rotation
 RotateBlit PROC uses eax ebx ecx edx esi lpBmp:PTR EECS205BITMAP, xcenter:DWORD, ycenter:DWORD, angle:FXPT
     LOCAL cosa:FXPT, sina:FXPT, shiftX:SDWORD, shiftY:SDWORD, dstWidth:DWORD, dstX:DWORD, dstY:DWORD
 
@@ -351,6 +475,15 @@ SKIP0:
     jmp DONE
 
 SKIP1:
+    ;; Use LeftBlit for about one-quarter rotations
+    cmp angle, 102943+1608
+    jg SKIP2
+    cmp angle, 102943-1608
+    jl SKIP2
+    INVOKE LeftBlit, lpBmp, xcenter, ycenter
+    jmp DONE
+
+SKIP2:    
     ;; Okay, this part is weird.
     ;; I admit it. I have no excuse, only an explanation.
     ;; When I got RotateBlit working, the star rotated in the opposite direction of the arrow key.
