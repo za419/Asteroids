45905d2d6515b789cd9a71136c01030cb5d7d7a7
Ryan Hodin <ryan951357@gmail.com>
2018-03-12 00:14:38 -0500

    Increase initial asteroid respawn time
    It was still too fast, even post #1

--- game.asm
+++ game.asm
@@ -749,7 +749,7 @@ endgame GameObject <0, 0, 00500000h, ZERO, ZERO, ZERO, ZERO, COLLISION_IGNORE, 0
 ;; Tags have value equal to about 2 seconds at 3 GHz (2.1 billion clock cycles)
 ;; Initial asteroid respawns as asteroid0
 ;; Flags set are 192, or RESPAWNING_OBJECT|KILL_SCORE
-asteroid_initial GameObject <OFFSET asteroid_003, 00a00000h, 01680000h, HALF, -HALF, ZERO, EPSILON+ROT_INC, 192, 0, 7e11d600h, 5, OFFSET asteroid0>
+asteroid_initial GameObject <OFFSET asteroid_003, 00a00000h, 01680000h, HALF, -HALF, ZERO, EPSILON+ROT_INC, 192, 1, 7e11d600h, 5, OFFSET asteroid0>
 
 ;; Template object for general asteroid
 ;; Tags have value equal to about 5 seconds at 3 GHz (15 billion clock cycles)
