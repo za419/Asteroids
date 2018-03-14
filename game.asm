a9e91d78ba3830a098a4682add9e1403ee599111
Ryan Hodin <ryan951357@gmail.com>
2018-03-13 20:25:34 -0500

    Add asteroid_002 sprite
    I would use 004, but, uh... It's rather phallic. With both the tree and
    boulders...............

--- game.asm
+++ game.asm
@@ -4573,6 +4573,43 @@ asteroid_001 EECS205BITMAP <32, 32, 255,, offset asteroid_001 + sizeof asteroid_
 
 
 
+asteroid_002 EECS205BITMAP <13, 14, 255,, offset asteroid_002 + sizeof asteroid_002>
+	BYTE 0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh
+	BYTE 0ffh,0ffh,049h,024h,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,049h,049h
+	BYTE 049h,024h,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,049h,091h,049h,024h,024h,024h
+	BYTE 024h,0ffh,0ffh,0ffh,0ffh,0ffh,091h,091h,0b6h,091h,049h,024h,024h,024h,024h,024h
+	BYTE 0ffh,0ffh,091h,0b6h,0b6h,091h,091h,091h,049h,049h,024h,049h,024h,024h,0ffh,091h
+	BYTE 0b6h,091h,091h,091h,0b6h,091h,091h,049h,049h,049h,024h,0ffh,091h,0b6h,0b6h,091h
+	BYTE 049h,091h,0b6h,091h,049h,024h,049h,024h,0ffh,0ffh,091h,0b6h,0b6h,091h,049h,091h
+	BYTE 049h,049h,024h,024h,024h,0ffh,0ffh,091h,0b6h,0b6h,0b6h,091h,091h,091h,049h,049h
+	BYTE 024h,024h,0ffh,0ffh,0ffh,091h,091h,0b6h,0b6h,0b6h,091h,091h,091h,091h,024h,0ffh
+	BYTE 0ffh,0ffh,0ffh,0ffh,091h,091h,0b6h,0b6h,0b6h,049h,049h,0ffh,0ffh,0ffh,0ffh,0ffh
+	BYTE 0ffh,0ffh,0ffh,091h,091h,091h,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh,0ffh
+	BYTE 0ffh,0ffh,0ffh,0ffh,0ffh,0ffh
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
